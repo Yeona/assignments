1d9eec7193ed69e2df991fef48bb981995b59426
Yeona <jaestableford@Jaes-MacBook-Pro.local>
2015-11-17 10:55:37 -1000

    week12

--- week-12/exercise/script.v2.js
+++ week-12/exercise/script.v2.js
@@ -1,9 +1,21 @@
 // Create an array including the first names of each student in the class
-var classMates;
+
+var classMates = ['Sam', 'Sasha', 'Heyla', 'Volha', 'Mayu', 'Myoung', 'Yoshi', 'Mark', 'Jae', 'Kalena'];
+
+var student = {
+  studentName: ['Sam', 'Mark'],
+  absentToday: true
+};
 
 // Student status string
 var studentStatus = ' is an interface design student.';
 
 // Display in the console the status of each student,
-// using a for loop and one console statement. 
-console.log(classMates + studentStatus);
+// using a for loop and one console statement.
+
+//console.log(classMates.length);
+//for(var i = 0; classmates.length){}
+
+for (var i = 0; i < classMates.length; i++){
+console.log(classMates[i] + studentStatus);
+}
